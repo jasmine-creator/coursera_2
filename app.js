4c1203ae1e84a43fc3ca89e32901928a9e26503f
jasmine <jasmine@test.com>
2020-06-30 09:17:45 +0100

    first web page

--- app.js
+++ app.js
@@ -1,48 +1,35 @@
 (function () {
-'use strict';
-//Enjoy!" ou "Trop! Veuillez saisir les données en premier
-angular.module('LunchCheck', [])
-.controller('LunchCheckController',LunchCheckController);
+  angular
+    .module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  
-  var chaine =document.getElementById("lunch-menu").value;
-   console.log(chaine);
+  LunchCheckController.$inject = ["$scope"];
 
-  $scope.check = function () {
-  var chaine =document.getElementById('lunch-menu').value;
-  const words = chaine.split(',');
-//console.log(words[3]);
-var nbr =words.length;
+  function LunchCheckController($scope) {
+    $scope.foodItems = "";
+    $scope.numFoodItems = 0;
+    $scope.message = "";
 
- if(chaine==""){
- 	
- 	$scope.result = "Veuillez saisir les données en premier";
- 	// scope.result.color='#FF0000'; 
- 	 document.getElementById('coul').style.color='#FF0000';
- 	
- } else if(nbr<=3){
-$scope.result = "Enjoy!"; 
-//document.getElementById('lunch-menu').style.color = ' #00FF00 ';
-document.getElementById('coul').style.color='#00FF00';
+    $scope.checkFoodItems = function () {
+      console.log("checkFoodItems()");
+      $scope.numFoodItems = 0;
+      let foodItems = $scope.foodItems.split(",");
+      foodItems.forEach((item) => {
+        if (item !== "") {
+          $scope.numFoodItems += 1;
+        }
+      });
+      $scope.message = $scope.getMessage();
+    };
 
- }else if(nbr>3){
-$scope.result = "Trop!"; 
-//document.getElementById('lunch-menu').style.color = ' #00FF00 ';
-document.getElementById('coul').style.color='#00FF00';
-
- }
-//placeholder="list comma separated dishes you usually have for lunch"
-  
-    
-  };
-
- 
-
-
-
-
-}
-
+    $scope.getMessage = function () {
+      let message = "Enjoy";
+      if ($scope.numFoodItems > 3) {
+        message = "Too much!";
+      } else if ($scope.numFoodItems === 0) {
+        message = "Please enter data first.";
+      }
+      return message;
+    };
+  }
 })();
